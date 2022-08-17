b635b17dd15f0f5eed821dfe69614a9d06739614
okedeji oluwatofunmi <oketofoke@gmail.com>
2022-08-17 06:03:49 +0100

    add static director

--- seepspring/settings.py
+++ seepspring/settings.py
@@ -136,9 +136,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
