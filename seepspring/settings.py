f33a409fdd2ff1728ff0f9bf0a18a80cd954c4fa
21toffy <oketofoke@gmail.com>
2022-09-18 01:16:44 +0300

    add seepspring to cors

--- seepspring/settings.py
+++ seepspring/settings.py
@@ -209,7 +209,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:5335',
-    'https://seepspringfe.heroku.app'
+    'https://seepspringfe.netlify.app',
+    'https://seepspring.netlify.app'
+
 ]
 
 
